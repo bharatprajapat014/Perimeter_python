5cbb1793b3d0c515a0685bbf81fb7d5c0fc153f4
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 10:44:23 +0530

    Create PerimeterOfCirclee.py

---
+++ PerimeterOfCirclee.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the parallelogram: "))
+width = float(input("Enter the width of the parallelogram: "))
+
+perimeter = 2 * (length + width)
+
+print("The perimeter of the parallelogram is:", perimeter)
